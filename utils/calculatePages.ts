39e0c84ed1d4d96b9c868bfc58f158e28e0e942c
Tengku Hafidz <tengkuhafidz@gmail.com>
2023-03-18 07:20:48 +0800

    finalise timezone method

--- utils/calculatePages.ts
+++ utils/calculatePages.ts
@@ -1,15 +1,11 @@
 import dayjs from "https://esm.sh/dayjs";
 import customParseFormat from "https://esm.sh/dayjs/plugin/customParseFormat";
 import utc from "https://esm.sh/dayjs/plugin/utc";
-import timezone from "https://esm.sh/dayjs/plugin/timezone";
 
 import { parseKhatamDate } from "./date.ts";
 
 dayjs.extend(customParseFormat)
 dayjs.extend(utc)
-dayjs.extend(timezone)
-const localTimezone = 'Asia/Singapore'
-const dayjsLocal = dayjs().tz(localTimezone)
 
 export const calculateDailyPages = (khatamDate: string, pagesRead: number, khatamPages: number) => {
     console.log("Calculating daily pages left...", khatamDate, pagesRead, khatamPages)
@@ -32,11 +28,6 @@ export const calculateKhatamCount = (pagesRead: number, khatamPages: number) => 
 }
 
 export const calculateDaysLeft = (khatamDate: string) => {
-    console.log("curr day", dayjs().format())
-    console.log("with utcOffset", dayjs().utcOffset(8).format())
-    console.log("with utcOffset start of day", dayjs().utcOffset(8).startOf('day').format())
-    console.log("local timezone", dayjsLocal.format())
-
     return parseKhatamDate(khatamDate).diff(dayjs().utcOffset(8).startOf('day'), "day")
 }
 
