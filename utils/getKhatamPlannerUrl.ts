40552464633af2c96949694a160166ab75ea17ed
Tengku Hafidz <tengkuhafidz@gmail.com>
2023-03-28 23:14:24 +0800

    fix bad link

--- utils/getKhatamPlannerUrl.ts
+++ utils/getKhatamPlannerUrl.ts
@@ -1,3 +1,3 @@
 export const getKhatamPlannerUrl = (daysLeft: number, totalPagesRead: number) => {
-    return `https://khatam-planner.jariyah.app?days=${daysLeft}startingPage=${totalPagesRead + 1}`
+    return `https://khatam-planner.jariyah.app?days=${daysLeft}&startingPage=${totalPagesRead + 1}`
 }
